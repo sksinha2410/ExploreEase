e0b6d3544b6223509aa2a770de4fa14fc32a3cb8
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-04-07 01:54:37 +0530

    Navigation dependency added and some bugs fixed

--- app/src/main/java/com/sksinha2410/exploreease/Activities/MainActivity.kt
+++ app/src/main/java/com/sksinha2410/exploreease/Activities/MainActivity.kt
@@ -1,16 +1,82 @@
 package com.sksinha2410.exploreease.Activities
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.sksinha2410.exploreease.R
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.audiofx.BassBoost
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.messaging.FirebaseMessaging
+import android.Manifest;
+import android.annotation.SuppressLint
+import android.provider.Settings
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNav: BottomNavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var toolbar: Toolbar
+    var permission_notif = false
+    lateinit var permissions: Array<String>
+    lateinit var notification: ImageView
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notification = findViewById(R.id.notification)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissions = arrayOf<String>(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
 
+        FirebaseMessaging.getInstance().subscribeToTopic("All")
+            .addOnCompleteListener(OnCompleteListener<Void?> { task ->
+                var msg = "Done"
+                if (!task.isSuccessful) {
+                    msg = "Failed"
+                }
+            })
+        FirebaseMessaging.getInstance().subscribeToTopic("/topics/All")
+        FirebaseMessaging.getInstance().subscribeToTopic("topics/All")
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        bottomNav = findViewById(R.id.bottom_nav_view)
+        navController = findNavController(R.id.fragmentContainerView)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.fragment_Home,
+                R.id.framentFeed,
+                R.id.liveEventsFragment,
+                R.id.profileFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNav.setupWithNavController(navController)
     }
 }
