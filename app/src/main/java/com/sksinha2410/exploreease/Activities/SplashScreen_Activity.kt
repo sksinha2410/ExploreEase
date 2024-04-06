42d63dd0a1e4d13d1a05db7caee7371ac74e23e6
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-04-06 20:12:48 +0530

    Splash Screen Started

--- app/src/main/java/com/sksinha2410/exploreease/Activities/SplashScreen_Activity.kt
+++ app/src/main/java/com/sksinha2410/exploreease/Activities/SplashScreen_Activity.kt
@@ -1,21 +1,50 @@
 package com.sksinha2410.exploreease.Activities
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.ingray.samagam.R
 import com.sksinha2410.exploreease.R
 
 class SplashScreen_Activity : AppCompatActivity() {
+    private lateinit var start:TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        start = findViewById(R.id.start)
+        start.setOnClickListener{
+            val mainIntent = Intent(this, Login_Activity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = Firebase.auth.currentUser
+        if(FirebaseAuth.getInstance().currentUser?.isEmailVerified == true){
+            if (currentUser != null) {
+                var intent:Intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(
+                    applicationContext,
+                    "Email not Verified",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
         }
     }
 }
