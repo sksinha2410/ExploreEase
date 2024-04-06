edbc5251731d0fbd645f840ce954b6dd50df8d07
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-04-07 00:27:21 +0530

    Guide Fee added

--- app/src/main/java/com/sksinha2410/exploreease/Activities/GuideRegister_Activity.kt
+++ app/src/main/java/com/sksinha2410/exploreease/Activities/GuideRegister_Activity.kt
@@ -1,5 +1,6 @@
 package com.sksinha2410.exploreease.Activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
@@ -14,8 +15,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.sksinha2410.exploreease.R
 import java.net.Inet4Address
 
@@ -25,11 +29,13 @@ class GuideRegister_Activity : AppCompatActivity() {
     private lateinit var location:EditText
     private lateinit var pincode:EditText
     private lateinit var description:EditText
+    private lateinit var charge:EditText
     private lateinit var register: Button
     private lateinit var back: Button
     private lateinit var id: ImageView
     private var language:String = ""
     private var dref: DatabaseReference = FirebaseDatabase.getInstance().getReference("Guide")
+    private var duref: DatabaseReference = FirebaseDatabase.getInstance().getReference("Users")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +49,20 @@ class GuideRegister_Activity : AppCompatActivity() {
         description = findViewById(R.id.etDescription)
         back = findViewById(R.id.ivBack)
         id = findViewById(R.id.ivID)
+        charge = findViewById(R.id.etCharge)
+        val currUser = FirebaseAuth.getInstance().currentUser?.uid
+
+        duref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.hasChild(currUser.toString())) {
+                    name.text = snapshot.child(currUser.toString()).child("name").value.toString()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@GuideRegister_Activity, "Error", Toast.LENGTH_SHORT).show()
+            }
+        })
 
 
         val radioButton1: RadioButton = findViewById(R.id.hindi)
@@ -57,7 +77,8 @@ class GuideRegister_Activity : AppCompatActivity() {
             if (radioButton2.isChecked) {
                 language+=radioButton2.text.toString()+","
             }
-            val b:Boolean=checkAllTheConditions(contact.text.toString(),location.text.toString(),pincode.text.toString(),description.text.toString())
+            val b:Boolean=checkAllTheConditions(contact.text.toString(),location.text.toString()
+                ,pincode.text.toString(),description.text.toString())
 
             if(b) {
                 val map: Map<String, Any> = mapOf(
@@ -66,9 +87,11 @@ class GuideRegister_Activity : AppCompatActivity() {
                     "location" to location.text.toString(),
                     "pincode" to pincode.text.toString(),
                     "description" to description.text.toString(),
-                    "language" to language
+                    "language" to language,
+                    "verified"  to "false",
+                    "charge" to charge.text.toString()
                 )
-                val currUser = FirebaseAuth.getInstance().currentUser?.uid
+
                 if (currUser != null) {
                     dref.child(currUser).setValue(map)
                 }
