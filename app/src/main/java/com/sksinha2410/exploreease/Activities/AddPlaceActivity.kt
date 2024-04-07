cd930a2052271ad60802a1edd9d7aca2226973c1
kush-raj-nit061 <kushpvt1@gmail.com>
2024-04-07 16:00:21 +0530

    Add Place added

---
+++ app/src/main/java/com/sksinha2410/exploreease/Activities/AddPlaceActivity.kt
@@ -0,0 +1,21 @@
+package com.sksinha2410.exploreease.Activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sksinha2410.exploreease.R
+
+class AddPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_place)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
